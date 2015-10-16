2530af0b0d884ab5eb5e5df1b732f2c698b3820c
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-10-16 00:19:35 -0400

    Add multiple class predictor

--- SimpleNB.py
+++ SimpleNB.py
@@ -96,11 +96,51 @@ def naive_bayes2(X, Y, gamma=1, verbose=False):
         theta[j,1] = (np.dot(X[:,j], Y) + gamma) / ( float(np.sum(Y==1)) + gamma * t )
         theta[j,0] = (np.dot(X[:,j], 1-Y) + gamma) / ( float(np.sum(Y==0)) + gamma * t)
 
-    def predictor(X, verbose=False):
+    def predictor(X_test, verbose=False, only_probabilities=False):
         to_return = []
-        for to_classify in X:
+        for to_classify in X_test:
             prob_y_0 = prior[0] * np.prod( np.multiply(to_classify, theta[:,0]) + np.multiply(1-to_classify, 1-theta[:,0]) )
             prob_y_1 = prior[1] * np.prod( np.multiply(to_classify, theta[:,1]) + np.multiply(1-to_classify, 1-theta[:,1]) )
-            to_return.append( [( 0, prob_y_0), (1, prob_y_1)] )
+            if not only_probabilities:
+                to_return.append( [( 0, prob_y_0), (1, prob_y_1)] )
+            else:
+                to_return.append( [ prob_y_0, prob_y_1] )
         return to_return
     return predictor
+
+def multiple_naive_bayes(X,Y, gamma=1, verbose=False):
+    number_of_classes = int(np.max(Y)) # zero counts as a class
+    # n = X.shape[0]
+
+    predictors = [None]*(number_of_classes+1)
+
+    for class_index in range(0, number_of_classes+1):
+        # print('class=',class_index)
+        single_class = Y.copy()
+        # handles when the class to predict is the zero class
+        single_class[single_class != class_index] = -1
+        single_class[single_class == class_index] = 1
+        single_class[single_class == -1 ] = 0
+
+        predictors[class_index] = naive_bayes2(X, single_class)
+
+
+    def predictor(X_test, verbose=False, only_probabilities=False):
+        
+        n = X_test.shape[0]
+
+        probability_predictions_matrix =  np.zeros((n, number_of_classes+1))
+
+        # keeping it verbose for readability
+        for class_index in range(0, number_of_classes+1):
+            # save our probabilities into a prediction matrix            
+            probability_predictions_matrix[:, class_index] = np.array(predictors[class_index](X_test, only_probabilities=True))[:,1] # 1 = selecting the positive class
+
+        if not only_probabilities:
+            return np.argmax(probability_predictions_matrix, axis=1)
+        else:
+            return probability_predictions_matrix
+
+    return predictor
+
+
