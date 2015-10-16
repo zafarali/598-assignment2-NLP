19c7aea413afac50900cf4cf529b98d94223e820
stuart spence <stuart.a.spence@gmail.com>
2015-10-16 19:18:18 -0400

    pass tests with new interface

--- stuart/tests/test_gimli.py
+++ stuart/tests/test_gimli.py
@@ -6,7 +6,9 @@ from utilities import *
 class TestGimli(unittest.TestCase):
 
     def test_process_data(self):
-        gimli=Gimli("tests/test1.csv",silent=1,filter_threshold=0,validation_ratio=1)
+        gimli=Gimli("tests/test1.csv",silent=1,validation_ratio=1)
+        gimli.process(filter_threshold=0)
+
         ngram=("b","c")
         result=gimli.ngram_count.get(ngram,0)
         expected=[2,1,0,0]
@@ -17,7 +19,9 @@ class TestGimli(unittest.TestCase):
         self.assertEqual(expected,result)
 
     def test_prediction(self):
-        gimli=Gimli("tests/test1.csv",filter_threshold=0,silent=1,validation_ratio=1)
+        gimli=Gimli("tests/test1.csv",silent=1,validation_ratio=1)
+        gimli.process(filter_threshold=0)
+
         text="a b c a b c"
         result=gimli.get_prediction(text)
         expected=0
