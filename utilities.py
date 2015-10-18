df190dfc3c259e1d8f27d49cfa147a88d872015c
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-10-17 22:33:33 -0400

    Add implementation of multiclass confusion matrix

--- utilities.py
+++ utilities.py
@@ -86,3 +86,89 @@ def print_color(text,color=0,end="\n"):
         prefix+="\033[%sm"%(color-10)
 
     print(prefix+text+"\033[0m",end=end)
+
+class ConfusionMatrix(object):
+    def __init__(self, actual, predicted):
+        """
+            Creates a confusion matrix
+        """
+        self.classes = np.unique(actual)
+        self.num_classes = len(self.classes)
+        confusion_matrix = np.zeros( ( self.num_classes, self.num_classes ) )
+
+        for i, j in combinations_with_replacement(self.classes, r=2):
+        #     print('i=',i,'j=',j)
+            confusion_matrix[i, j] = np.sum(np.logical_and(ACTUAL == i, PREDICT == j))
+        #     print(confusion_matrix[i-1, j-1])
+
+            confusion_matrix[j, i] = np.sum(np.logical_and(ACTUAL == j, PREDICT == i))
+
+        self.confusion_matrix = confusion_matrix
+            
+    def TP(self, label=1):
+        """
+            Returns the True Positives for Label provided
+        """
+        return self.confusion_matrix[label, label]
+    
+    def FN(self, label=1):
+        """
+            Returns the False Negatives for Label Provided
+        """
+        trues = self.TP(label)
+        false_negs = np.sum(self.confusion_matrix[label,:]) - trues
+        
+        return false_negs
+    
+    def FP(self, label=1):
+        """
+            Returns the False Positives for Label Provided
+        """
+        trues = self.TP(label)
+        false_pos = np.sum(self.confusion_matrix[:,label]) - trues
+        return false_pos
+    
+    def TN(self, label=1):
+        """
+            Returns the True Negatives for Label Provided
+        """
+        total = np.sum(self.confusion_matrix)
+        total = total - self.FP(label) - self.FN(label) - self.TP(label)
+        
+        return total
+    
+    def accuracy(self, label=1):
+        """
+            Returns the Accuracy for the Label Provided
+        """
+        return (self.TP(label)+self.TN(label))/float(self.TP(label)+self.TN(label)+self.FP(label)+self.FN(label))
+        
+    def average_accuracy(self):
+        accuracy = 0
+        for label in self.classes:
+            accuracy += self.accuracy(label)
+        
+        return accuracy/float(self.num_classes)
+    
+    def precision(self):
+        numerator, denominator = 0.0, 0.0
+        
+        for label in self.classes:
+            numerator += self.TP(label)
+            denominator += self.TP(label) + self.FP(label)
+            
+        return (numerator/denominator)
+    
+    def recall(self):
+        
+        numerator, denominator= 0.0, 0.0
+        
+        for label in self.classes:
+            numerator += self.TP(label)
+            denominator += self.TP(label) + self.FN(label)
+            
+        return (numerator/denominator)
+
+        
+        
+        
